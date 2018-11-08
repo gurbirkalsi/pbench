7485610943321210304564b6e96e8147badcba10
Gurbir Kalsi <kalsigurbir@gmail.com>
2018-11-08 10:32:08 -0500

    Update boilerplate language

--- web-server/pdash/src/utils/request.js
+++ web-server/pdash/src/utils/request.js
@@ -4,21 +4,21 @@ import { routerRedux } from 'dva/router';
 import store from '..';
 
 const codeMessage = {
-  200: '服务器成功返回请求的数据。',
-  201: '新建或修改数据成功。',
-  202: '一个请求已经进入后台排队（异步任务）。',
-  204: '删除数据成功。',
-  400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
-  401: '用户没有权限（令牌、用户名、密码错误）。',
-  403: '用户得到授权，但是访问是被禁止的。',
-  404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
-  406: '请求的格式不可得。',
-  410: '请求的资源被永久删除，且不会再得到的。',
-  422: '当创建一个对象时，发生一个验证错误。',
-  500: '服务器发生错误，请检查服务器。',
-  502: '网关错误。',
-  503: '服务不可用，服务器暂时过载或维护。',
-  504: '网关超时。',
+  200: 'The server successfully returned the requested data. ',
+  201: 'New or modified data is successful. ',
+  202: 'A request has entered the background queue (asynchronous task). ',
+  204: 'The data was deleted successfully. ',
+  400: 'The request returned an error and the server did not perform any new or modified data operations. ',
+  401: 'User does not have permission (token, username, or password is incorrect). ',
+  403: 'The user is authorized, but access is forbidden. ',
+  404: 'The request was made for a record that does not exist. ',
+  406: 'The format of the request is not available. ',
+  410: 'The requested resource is permanently deleted and will not be retrieved. ',
+  422: 'A validation error occurred when creating an object. ',
+  500: 'An error occurred on the server. Please check the server. ',
+  502: 'Gateway error. ',
+  503: 'The service is unavailable and the server is temporarily overloaded or maintained. ',
+  504: 'The gateway timed out. ',
 };
 function checkStatus(response) {
   if (response.status >= 200 && response.status < 300) {
@@ -26,7 +26,7 @@ function checkStatus(response) {
   }
   const errortext = codeMessage[response.status] || response.statusText;
   notification.error({
-    message: `请求错误 ${response.status}: ${response.url}`,
+    message: `Request error ${response.status}: ${response.url}`,
     description: errortext,
   });
   const error = new Error(errortext);
